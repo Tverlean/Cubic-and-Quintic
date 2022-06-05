20edef5b4912e001b01b6e8f595cf10771cb467b
Tverlean <106289949+Tverlean@users.noreply.github.com>
2022-06-05 18:01:40 +0700

    Added project
    Cubic and Quintic for Cylindrical Manipulator

---
+++ Cubic_P.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# degrees to radian convert
+def mm_to_m(d):
+    m=1000
+    return d/m
+
+di = float(input('di =  ')) #initial position
+di = mm_to_m(di)
+
+vi = float(input('vi =  ')) #initial velocity
+vi = mm_to_m(vi)
+
+df = float(input('df =  ')) #final position
+df = mm_to_m(df)
+
+vf = float(input('vf =  ')) #final velocity
+vf = mm_to_m(vf)
+
+ti = float(input('ti =  ')) #initial time
+tf = float(input('tf =  ')) #final time
+
+#cubic
+#Solve the soln for q(t) = a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3
+
+x = np.arange(ti,tf,0.05)
+
+def cubic(t,a,b,c): 
+    return a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3
+
+y = cubic(x,di,df,tf)
+
+plt.figure()
+plt.plot(x,y,'r',linestyle='-')
+plt.text(1,1.5,'q(t) = a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3')
+plt.grid(True)
+plt.show()
